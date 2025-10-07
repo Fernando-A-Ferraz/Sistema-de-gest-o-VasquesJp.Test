f33940a2aa25d6ecd9154af63c3848d2438b8ed9
Fernando Augusto Ferraz <100580357+Fernando-A-Ferraz@users.noreply.github.com>
2025-10-06 23:53:25 -0300

    Mask USER_PASSWORD in cypress.config.js
    Updated USER_PASSWORD to be hidden for security.

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
       env: {
       USER_EMAIL: "admin@vasques.dev",
-      USER_PASSWORD: "123456"
+      USER_PASSWORD: "******"
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
